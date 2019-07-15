3eef056fc472062fbe15f35202607ec7937fda54
Nicolas Audebert <nicolas.audebert@quicksign.com>
2019-07-15 18:06:23 +0200

    Refactor main algorithm
    This change removes the gradient computation and iterative diffusion (for one step) in separate functions.
    It also removes a dead function that has been inlined.

--- GAD.py
+++ GAD.py
@@ -1,8 +1,9 @@
-# Guided Anisotropic Diffusion algorithm
-# Rodrigo Caye Daudt
-# https://rcdaudt.github.io
-#
-# Caye Daudt, Rodrigo, Bertrand Le Saux, Alexandre Boulch, and Yann Gousseau. "Guided anisotropic diffusion and iterative learning for weakly supervised change detection." In Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition Workshops, pp. 0-0. 2019.
+# TensorFlow implementation of the Guided Anisotropic Diffusion algorithm
+# Original PyTorch implementation by Rodrigo Caye Daudt (https://rcdaudt.github.io)
+# Reference paper:
+# Caye Daudt, Rodrigo, Bertrand Le Saux, Alexandre Boulch, and Yann Gousseau.
+# "Guided anisotropic diffusion and iterative learning for weakly supervised change detection."
+# In Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition Workshops, 2019.
 
 import tensorflow as tf
 
@@ -14,8 +15,6 @@ def g(x, K=5):
 def get_gradients(image):
     dv = image[:, :, 1:, 1:-1] - image[:, :, :-1, 1:-1]
     dh = image[:, :, 1:-1, 1:] - image[:, :, 1:-1, :-1]
-    # dv = image[:, :, 1:, :] - image[:, :, :-1, :]
-    # dh = image[:, :, :, 1:] - image[:, :, :, :-1]
     return dv, dh
 
 
@@ -62,61 +61,15 @@ def anisotropic_diffusion(
 
         # Perform diffusion on the first guide
         cv1, ch1 = diffusion_coefficient(first_guide_image, K=K)
-
-        channels = []
-
-        # Compute gradients from the guide image
-        dv = first_guide_image[:, :, 1:, 1:-1] - first_guide_image[:, :, :-1, 1:-1]
-        dh = first_guide_image[:, :, 1:-1, 1:] - first_guide_image[:, :, 1:-1, :-1]
-        for channel in range(int(first_guide_image.shape[1])):
-            diffused_channel = first_guide_image[:, channel, 1:-1, 1:-1] + lambda_ * (
-                cv1[:, 1:, :] * dv[:, channel, 1:, :]
-                - cv1[:, :-1, :] * dv[:, channel, :-1, :]
-                + ch1[:, :, 1:] * dh[:, channel, :, 1:]
-                - ch1[:, :, :-1] * dh[:, channel, :, :-1]
-            )
-            channels.append(diffused_channel)
-        first_guide_image = tf.pad(
-            tf.stack(channels, axis=1), tf.constant([[0, 0], [0, 0], [1, 1], [1, 1]])
-        )
-        del (dv, dh)
+        first_guide_image = diffuse(cv1, ch1, first_guide_image, lambda_)
 
         cv2, ch2 = diffusion_coefficient(second_guide_image, K=K)
-
-        channels = []
-        dv = second_guide_image[:, :, 1:, 1:-1] - second_guide_image[:, :, :-1, 1:-1]
-        dh = second_guide_image[:, :, 1:-1, 1:] - second_guide_image[:, :, 1:-1, :-1]
-        for channel in range(int(second_guide_image.shape[1])):
-            diffused_channel = second_guide_image[:, channel, 1:-1, 1:-1] + lambda_ * (
-                cv2[:, 1:, :] * dv[:, channel, 1:, :]
-                - cv2[:, :-1, :] * dv[:, channel, :-1, :]
-                + ch2[:, :, 1:] * dh[:, channel, :, 1:]
-                - ch2[:, :, :-1] * dh[:, channel, :, :-1]
-            )
-            channels.append(diffused_channel)
-        second_guide_image = tf.pad(
-            tf.stack(channels, axis=1), tf.constant([[0, 0], [0, 0], [1, 1], [1, 1]])
-        )
-        del (dv, dh)
+        second_guide_image = diffuse(cv2, ch2, second_guide_image, lambda_)
 
         cv = tf.math.minimum(cv1, cv2)
         ch = tf.math.minimum(ch1, ch2)
         del (cv1, ch1, cv2, ch2)
 
-        dv = input_image[:, :, 1:, 1:-1] - input_image[:, :, :-1, 1:-1]
-        dh = input_image[:, :, 1:-1, 1:] - input_image[:, :, 1:-1, :-1]
-        channels = []
-        for channel in range(int(input_image.shape[1])):
-            diffused_channel = input_image[:, channel, 1:-1, 1:-1] + lambda_ * (
-                cv[:, 1:, :] * dv[:, channel, 1:, :]
-                - cv[:, :-1, :] * dv[:, channel, :-1, :]
-                + ch[:, :, 1:] * dh[:, channel, :, 1:]
-                - ch[:, :, :-1] * dh[:, channel, :, :-1]
-            )
-            channels.append(diffused_channel)
-        input_image = tf.pad(
-            tf.stack(channels, axis=1), tf.constant([[0, 0], [0, 0], [1, 1], [1, 1]])
-        )
-        del (dv, dh)
+        input_image = diffuse(cv, ch, input_image, lambda_)
 
     return input_image
