de52bdb77c2bf847f227f11063676f0270b0a645
Nicolas Audebert <nicolas.audebert@quicksign.com>
2019-07-17 17:32:29 +0200

    Avoid image gradients recompute
    Prior to this change, image were computed both for coefficient computation and in the iterative diffusion step.
    
    This change refactors the functions so that image gradients are computed once for each image. This speeds up the diffusion.

--- GAD.py
+++ GAD.py
@@ -12,36 +12,44 @@ def g(x, K=5):
     return 1.0 / (1.0 + (tf.abs((x * x)) / (K * K)))
 
 
-def get_gradients(image):
+def get_image_gradients(image):
     dv = image[:, :, 1:, 1:-1] - image[:, :, :-1, 1:-1]
     dh = image[:, :, 1:-1, 1:] - image[:, :, 1:-1, :-1]
     return dv, dh
 
 
-def diffusion_coefficient(image, K=5):
-    dv, dh = get_gradients(image)
-    cv = g(tf.reduce_mean(dv, 1), K)
-    ch = g(tf.reduce_mean(dh, 1), K)
+def diffusion_coefficient(gradient_v, gradient_h, K):
+    cv = g(tf.reduce_mean(gradient_v, 1), K)
+    ch = g(tf.reduce_mean(gradient_h, 1), K)
     return cv, ch
 
 
-def diffuse(cv, ch, image, lambda_):
+def diffuse(image, lambda_, K, coeffs=None, return_coeffs=True):
     # Compute gradients from the image
-    dv, dh = get_gradients(image)
+    dv, dh = get_image_gradients(image)
+    if coeffs is None:
+        cv, ch = diffusion_coefficient(dv, dh, K)
+    else:
+        cv, ch = coeffs
     diffused = image[:, :, 1:-1, 1:-1] + lambda_ * (
         cv[:, 1:, :] * dv[:, :, 1:, :]
         - cv[:, :-1, :] * dv[:, :, :-1, :]
         + ch[:, :, 1:] * dh[:, :, :, 1:]
         - ch[:, :, :-1] * dh[:, :, :, :-1]
     )
-    del (dv, dh)
-    return tf.pad(diffused, tf.constant([[0, 0], [0, 0], [1, 1], [1, 1]]))
+    image = tf.pad(diffused, tf.constant([[0, 0], [0, 0], [1, 1], [1, 1]]))
+    if return_coeffs:
+        del(dv, dh, diffused)
+        return image, cv, ch
+    else:
+        del(dv, dh, cv, ch, diffused)
+        return image
 
 
 def anisotropic_diffusion(
     input_image,
-    first_guide_image,
-    second_guide_image=None,
+    first_guide,
+    second_guide=None,
     iterations=500,
     lambda_=0.24,
     K=5,
@@ -57,20 +65,18 @@ def anisotropic_diffusion(
             print("Iteration {}".format(t))
 
         # Perform diffusion on the first guide
-        cv1, ch1 = diffusion_coefficient(first_guide_image, K=K)
-        first_guide_image = diffuse(cv1, ch1, first_guide_image, lambda_)
+        first_guide, cv1, ch1 = diffuse(first_guide, lambda_, K, return_coeffs=True)
 
         # Perform diffusion on the second guide (if specified)
-        if second_guide_image is not None:
-            cv2, ch2 = diffusion_coefficient(second_guide_image, K=K)
-            second_guide_image = diffuse(cv2, ch2, second_guide_image, lambda_)
+        if second_guide is not None:
+            second_guide, cv2, ch2 = diffuse(second_guide, lambda_, K, return_coeffs=True)
 
             cv = tf.math.minimum(cv1, cv2)
             ch = tf.math.minimum(ch1, ch2)
-            del (cv1, ch1, cv2, ch2)
+            del(cv1, cv2, ch1, ch2)
         else:
             cv, ch = cv1, ch1
 
-        input_image = diffuse(cv, ch, input_image, lambda_)
+        input_image = diffuse(input_image, lambda_, K, coeffs=(cv, ch))
 
     return input_image
